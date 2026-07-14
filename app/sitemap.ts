c07a15ccf75f64d5c6ead731fd80949ed312d6b1
Orhan <orhanduzgn@gmail.com>
2026-07-15 00:53:37 +0300

    Revert "Restructure site into multi-page app and add admin CMS backend"
    This reverts commit a1424c0babfb69465a559d3e71264f0f972b61c2.

--- app/sitemap.ts
+++
@@ -1,8 +0,0 @@
-import type { MetadataRoute } from "next";
-import { publicRoutes, siteConfig } from "../lib/site-config";
-
-export default function sitemap(): MetadataRoute.Sitemap {
-  return publicRoutes.map((path) => ({
-    url: new URL(path, siteConfig.url).toString(),
-  }));
-}
--- drizzle.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "drizzle-kit";
-
-export default defineConfig({
-  out: "./drizzle",
-  schema: "./db/schema.ts",
-  dialect: "sqlite",
-});
-
